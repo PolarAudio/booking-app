97c56db1d4225c471ed00172829238296194cad0
MAXQON <216683641+PolarAudio@users.noreply.github.com>
2025-07-22 13:03:15 +0800

    smal fix

--- src/App.jsx
+++ src/App.jsx
@@ -589,7 +589,7 @@ function BookingApp() {
                 <div className="bg-red-800 p-8 rounded-lg shadow-xl w-full max-w-md border border-red-700 text-center">
                     <h2 className="text-3xl font-bold text-white mb-4">Server Offline or Under Maintenance</h2>
                     <p className="text-gray-200 mb-6">We are currently working on new features. Please try again in some Minutes.</p>
-					<p className="text-gray-200 mb-6">If the Page is not back online after 10 Minutes contact the <a href=”mailto:polarsolutions.warehouse@gmail.com”>Admin</a></p>
+					<p className="text-gray-200 mb-6">If the Page is not back online after 10 Minutes contact the <a href="mailto:polarsolutions.warehouse@gmail.com">Admin</a></p>
                 </div>
             </div>
         );
